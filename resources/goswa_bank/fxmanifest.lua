5bc9d299dc61adde50ee29194bc3ee52123646e9
neilouali200-eng <neil.ouali200@gmail.com>
2025-11-13 15:24:18 +0100

    Create fxmanifest.lua

---
+++ resources/goswa_bank/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Goswa'
+description 'Banque avec interface NUI'
+version '1.0.0'
+
+client_scripts {
+    'client.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js'
+}
