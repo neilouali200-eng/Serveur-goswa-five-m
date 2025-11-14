f47a3fa876fca475be4ef996ee4bf0203b591eb7
neilouali200-eng <neil.ouali200@gmail.com>
2025-11-14 09:23:26 +0100

    Create fxmanifest.lua

---
+++ resources/fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Goswa'
+description 'Concessionnaire RP avec catalogue + menu vendeur'
+version '1.0.0'
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
+
+ui_page "html/index.html"
+
+files {
+    "html/index.html",
+    "html/style.css",
+    "html/script.js"
+}
